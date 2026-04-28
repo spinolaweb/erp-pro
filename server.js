c4e2450e5d16ccc8a441fa46bc817e20c1c8bd98
RBX_ABDE <rbxabdeofficial@gmail.com>
2026-04-28 21:43:23 +0000

    Fix server.js syntax

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use('/api/backup', backupRouter);
 
 app.use(express.static(path.join(__dirname, 'src/dist')));
 
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/dist/index.html'));
 });
 
